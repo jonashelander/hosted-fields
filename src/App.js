423fc942b7fec245f6d6ecb5d6433ddf2b0119a5
jonashelander <helanderjonas@gmail.com>
2025-02-28 10:05:44 +0100

    Altered the callback and on load callback to return an actual callback to get rid of the error overlay that was thrown on initialization and when fetching the fields

--- src/App.js
+++ src/App.js
@@ -113,16 +113,8 @@ export default function App() {
     });
   };
 
-  // const handleGetFields = () => {
-  //   HostedFields.get();
-  // };
-
-  const handleGetFields = async () => {
-    try {
-      await HostedFields.get(); // Await the function if it's async
-    } catch (error) {
-      console.error("Error occurred while getting fields:", error);
-    }
+  const handleGetFields = () => {
+    HostedFields.get();
   };
 
   const handleCallback = () => {
@@ -136,22 +128,125 @@ export default function App() {
 
   useEffect(() => {
     if (hasInitialized.current) return;
-    setupMethod();
+
+    let fieldConfig = [
+      {
+        type: FieldTypes.TEXT,
+        id: "frmNameCC",
+        autocomplete: "cc-name",
+        key: "creditcard.main.name",
+        helpKey: "Name",
+        helpkey: "Name",
+        name: "ccname",
+        noAttributeValueFormatting: true,
+        required: true,
+        requiredNewPayment: true,
+        requiredRepeatPayment: false,
+        showIcon: true,
+        value: undefined,
+        visible: true,
+        visibleOnNewPayment: true,
+        visibleOnRepeatPayment: false,
+        defaultValue: "Jonas Helander",
+      },
+      {
+        type: FieldTypes.CREDITCARD_NUMBER,
+        autocomplete: "cc-number",
+        helpKey: "Kortnummer",
+        id: "frmCCNum",
+        key: "creditcard.main.number",
+        name: "cardnumber",
+        noAttributeValueFormatting: true,
+        required: true,
+        requiredNewPayment: true,
+        requiredRepeatPayment: false,
+        showIcon: true,
+        value: undefined,
+        visible: true,
+        visibleOnNewPayment: true,
+        visibleOnRepeatPayment: false,
+      },
+      {
+        type: FieldTypes.EXPIRY_MM_YYYY,
+        autocomplete: "cc-exp",
+        helpKey: "•• / ••",
+        id: "frmCCExp",
+        key: "creditcard.main.expirymmyyyy",
+        name: "cc-exp",
+        noAttributeValueFormatting: true,
+        pattern: "[0-9]*",
+        required: true,
+        requiredNewPayment: true,
+        requiredRepeatPayment: false,
+        showIcon: true,
+        value: undefined,
+        visible: true,
+        visibleOnNewPayment: true,
+        visibleOnRepeatPayment: false,
+      },
+      {
+        type: FieldTypes.CVV,
+        autocomplete: "cc-csc",
+        helpKey: "Security Code",
+        id: "frmCCCVC",
+        key: "creditcard.main.cvc",
+        name: "cvc",
+        noAttributeValueFormatting: true,
+        pattern: "[0-9]*",
+        required: true,
+        requiredNewPayment: true,
+        requiredRepeatPayment: true,
+        showIcon: true,
+        value: undefined,
+        visible: true,
+        visibleOnNewPayment: true,
+        visibleOnRepeatPayment: true,
+      },
+    ];
+    const setupHostedFields = async () => {
+      try {
+        HostedFields.setup({
+          merchantId: 2906,
+          renderMode: "single",
+          hostedfieldsurl:
+            // "https://test-hostedpages.paymentiq.io/1.0.31/index.html",
+            "https://test-hostedpages.paymentiq.io/1.0.51/index.html",
+
+          fields: fieldConfig,
+          service: "",
+          styles: "",
+          callback: (data) => {
+            return () => {
+              console.log("CALLBACK", data);
+            };
+          },
+          onLoadCallback: () => {
+            return () => {
+              console.log("ON LOAD CALLBACK");
+            };
+          },
+          autoFocusNext: true,
+          el: `#hosted-fields-wrapper`,
+        });
+      } catch (error) {
+        console.error("Error in HostedFields.setup:", error);
+      }
+    };
+    setupHostedFields();
     hasInitialized.current = true;
 
-    // Listen for messages from the Hosted Fields iframe
     const handleMessage = (event) => {
-      if (event.origin !== "https://test-hostedpages.paymentiq.io") return; // Ensure it's from a trusted source
-      console.log("Message received from Hosted Fields:", event.data.formData);
+      if (event.origin !== "https://test-hostedpages.paymentiq.io") return;
+      console.log("Message received from Hosted Fields:", event.data);
     };
     window.addEventListener("message", handleMessage);
-  }, []);
+  }, [setupMethod]);
 
   return (
     <div className="App">
       <h1>Hosted Fields</h1>
       <button onClick={handleGetFields}>Get fields</button>
-      <div id="hosted-fields-wrapper" />´
+      <div id="hosted-fields-wrapper" />
     </div>
   );
 }
